79ba9d95d75082cb1ea10be2698cb9eb85b396c2
Ralph <ralphoburu@gmail.com>
2021-03-19 12:34:52 +0300

    Add user assosistions

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,32 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  def friends
+    friends_array = friendships.map do |friendship|
+      friendship.friend if friendship.confirmed
+    end + inverse_friendships.map do |friendship|
+            friendship.user if friendship.confirmed
+          end
+
+    friends_array.compact
+  end
+
+  # Users who have yet to confirmed friend requests
+  def pending_friends
+    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
+  def requester?(user)
+    id == Friendship.inverse_friendships(self, user).first.requester_id
+  end
 end
