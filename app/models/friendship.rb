3616b46ff936490f0aaea8868ca02a4d265a9879
Ralph <ralphoburu@gmail.com>
2021-03-22 18:08:54 +0300

    Fix rubocop errors

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,10 +4,10 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   scope :friendship_exists,
-  ->(user, friend) { where(" (user_id = #{user.id} AND friend_id = #{friend.id})") }
+        ->(user, friend) { where(" (user_id = #{user.id} AND friend_id = #{friend.id})") }
 
-scope :inverse_friendships,
-  ->(user, friend) { where("( user_id = #{user.id} AND friend_id = #{friend.id}) OR ( user_id = #{friend.id} AND friend_id = #{user.id})") }
+  scope :inverse_friendships,
+        ->(user, friend) { where("( user_id = #{user.id} AND friend_id = #{friend.id}) OR ( user_id = #{friend.id} AND friend_id = #{user.id})") }
 
   def check_friendship
     if Friendship.friendship_exists(User.find(user_id), User.find(friend_id)).to_a.any?
